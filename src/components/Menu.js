eb80087615450cf878734390fa66e5b320a800e7
PauloMRC <paulo_mrcosta@msn.com>
2018-09-22 17:01:25 -0300

    corrigida herança do Grid

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,19 +5,19 @@ import login from './login.png';
 const Menu = () => (
   <header>
     <div class="header">
-      <div class="header logo">
+      <div className="logo">
         <a href="https://www.panorante.com.br" title="Panorante" id="brd-link" tabindex="6">
           <img width="80%" src={brand} className="App-logo" alt="brand" />
           <title id="brd-title">Panorante</title>
         </a>
       </div>
-      <div class="header menu">
+      <div className="menu">
         <div><a href="https://www.panorante.com.br/Perfil/Profissionais">Profissionais</a></div>
         <div><a href="https://www.panorante.com.br">Lojas</a></div>
         <div><a href="https://www.panorante.com.br">Serviços</a></div>
         <div><a href="https://www.panorante.com.br/Contato/Contato">Fale conosco</a></div>
       </div>
-      <div class="header login">
+      <div className="login">
         <a href="https://www.panorante.com.br" title="Login" id="brd-link" tabindex="6">
           <img width="80%" src={login} className="App-logo" alt="brand" />
           <title id="brd-title">Login</title>
